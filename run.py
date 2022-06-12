60d01be2c94228971092fc44f0c5c505277a1e53
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2022-06-12 16:10:54 +0200

    Add logs and fix Terminator error

--- run.py
+++ run.py
@@ -1,33 +1,34 @@
-from asyncio.log import logger
+import logging
 import turtle
 import time
-
+logging.basicConfig(filename='logs/app.log',
+                    encoding='utf-8', level=logging.INFO)
 if __name__ == '__main__':
     wn = turtle.Screen()
     wn.title("SUS")
     wn.bgcolor('skyblue')
     wn.setup(width=200, height=100)
-for i in range(10000000):
-    turtle.penup()
-    turtle.setpos(-30, 0)
-    turtle.pendown()
-    turtle.color("red")
-    turtle.write("SUS", move=True, align="left", font=("serif", 18))
-    time.sleep(0.5)
-    turtle.penup()
-    turtle.setpos(-30, 0)
-    turtle.pendown()
-    turtle.color("green")
-    turtle.write("SUS", move=True, align="left", font=("serif", 18))
-    time.sleep(0.5)
-    turtle.penup()
-    turtle.setpos(-30, 0)
-    turtle.pendown()
-    turtle.color("blue")
-    turtle.write("SUS", move=True, align="left", font=("serif", 18))
-    time.sleep(0.5)
-while True:
-    try:
+try:
+    for i in range(10000000):
+        turtle.penup()
+        turtle.setpos(-30, 0)
+        turtle.pendown()
+        turtle.color("red")
+        turtle.write("SUS", move=True, align="left", font=("serif", 18))
+        time.sleep(0.5)
+        turtle.penup()
+        turtle.setpos(-30, 0)
+        turtle.pendown()
+        turtle.color("green")
+        turtle.write("SUS", move=True, align="left", font=("serif", 18))
+        time.sleep(0.5)
+        turtle.penup()
+        turtle.setpos(-30, 0)
+        turtle.pendown()
+        turtle.color("blue")
+        turtle.write("SUS", move=True, align="left", font=("serif", 18))
+        time.sleep(0.5)
+    while True:
         wn.update()
-    except turtle.Terminator:
-        logger.log("App Terminated")
+except turtle.Terminator:
+    logging.info(msg="App Terminated")
